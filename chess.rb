903d146ca0b37de8c2f45f1497dc7ead4108ea95
App Academy Student <student@appacademy.io>
2013-09-24 16:15:47 -0700

    Display board & move for stepping pieces

--- chess.rb
+++ chess.rb
@@ -8,7 +8,6 @@ class Piece
   end
 
   def move(target_pos)
-    # raise "No can do" unless valid_move?(target_pos)
     board[self.pos] = nil
     self.pos = target_pos
     board[target_pos] = self
@@ -18,37 +17,6 @@ end
 
 
 class SlidingPiece < Piece
-  # def valid_move?(target_pos)
-#
-#     #in bounds on board, #valid direction, #not obstructed
-#     possible_move?(target_pos)
-#
-#     #doesn't leave own king in check
-#
-#
-#
-#     #target_pos doesn't have own piece
-#     own_piece?(target_pos)
-#
-#
-#   end
-#
-#   def king_compromised?(target_pos)
-#     copy_board = board.deep_dup
-#
-#     copy_board[target_pos] = self
-#     copy_board[self.pos] = nil
-#
-#     copy_board.each do |row|
-#       row.each do |piece|
-#         next if !piece.is_a? Piece
-#         next if piece.color == self.color
-#
-#
-#       end
-#     end
-#   end
-
 
   def possible_moves
 
@@ -70,20 +38,16 @@ class SlidingPiece < Piece
     possible_moves
   end
 
-
-  # def own_piece?(target_pos)
-#     board[target_pos].color == self.color
-#   end
-
-
-
-
 end
 
 class Queen < SlidingPiece
 
   def move_dir
     [[0, 1], [1, 0], [-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+  end
+
+  def render
+    "Q"
   end
 end
 
@@ -93,6 +57,10 @@ class Bishop < SlidingPiece
     [[1, 1], [-1, -1], [1, -1], [-1, 1]]
   end
 
+  def render
+    "B"
+  end
+
 end
 
 class Rook < SlidingPiece
@@ -100,16 +68,38 @@ class Rook < SlidingPiece
   def move_dir
     [[0, 1], [1, 0], [-1, 0], [0, -1]]
   end
+
+  def render
+    "R"
+  end
+
 end
 
 
 class SteppingPiece <Piece
+
+  def possible_moves
+
+    possible_moves = []
+
+    self.move_dir.each do |dir|
+      absolute_coord = [pos, dir].transpose.map { |el| el.reduce(&:+) }
+      possible_moves << absolute_coord if absolute_coord.all? { |coord| coord.between?(0,7) }
+    end
+
+    possible_moves
+  end
 end
 
 class Knight < SteppingPiece
   def move_dir
     [[2, 1], [2, -1], [1, 2], [1, -2], [-2, -1], [-2, 1], [-1, -2], [-1, 2]]
   end
+
+  def render
+    "H"
+  end
+
 end
 
 class Pawn < SteppingPiece
@@ -120,12 +110,22 @@ class Pawn < SteppingPiece
   def attack
     [[-1, 1], [1, 1]]
   end
+
+  def render
+    "P"
+  end
+
 end
 
 class King < SteppingPiece
   def move_dir
     [[0, 1], [1, 0], [-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
   end
+
+  def render
+    "K"
+  end
+
 end
 
 
@@ -144,9 +144,8 @@ class Board
   def set_board
     board[0] = Rook.new(self, [0, 0], "B"), Knight.new(self, [0, 1], "B"), Bishop.new(self, [0, 2], "B"), King.new(self, [0, 3], "B"), Queen.new(self, [0, 4], "B"), Bishop.new(self, [0, 5], "B"), Knight.new(self, [0, 6], "B"), Rook.new(self, [0, 7], "B")
     board[7] = Rook.new(self, [7, 0], "W"), Knight.new(self, [7, 1], "W"), Bishop.new(self, [7, 2], "W"), King.new(self, [7, 3], "W"), Queen.new(self, [7, 4], "W"), Bishop.new(self, [7, 5], "W"), Knight.new(self, [7, 6], "W"), Rook.new(self, [7, 7], "W")
-    # board[1] = Array.new(8) { (0..7).map {|col| Pawn.new(self, [0, col], "B")} }
     (0..7).each do |col|
-      # board[1][col] = Pawn.new(self, [0, col], "W")
+      #board[1][col] = Pawn.new(self, [0, col], "W")
       board[6][col] = Pawn.new(self, [0, col], "B")
     end
   end
@@ -210,13 +209,30 @@ class Board
   def moving_valid_color?
   end
 
+  def display
+    board.each do |row|
+      row.each do |piece|
+        print piece.is_a?(Piece) ? piece.render + " " : "- "
+      end
+      puts ""
+    end
+  end
 
 end
 
 
 class Game
+
 end
 
 
 class Player
 end
+
+b = Board.new
+b.display
+b.move([0,1],[1,3])
+b.display
+b.move([1,3],[2,5])
+b.display
+b.move([2,5],[4,7])
