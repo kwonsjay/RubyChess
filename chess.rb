2c15e4bb11aa5878d56796dcbe4fa38a0a45f24a
SoonJae Kwon <kwonsjay@gmail.com>
2013-11-21 12:00:13 -0800

    Update chess.rb

--- chess.rb
+++ chess.rb
@@ -61,31 +61,7 @@ end
 
 g = Game.new
 g.play
-# b = Board.new
-# b.display
-# b.move("W", [7,7],[0,7])
-# b.display
-# b.move("W", [0,7],[0,6])
-# b.display
-# b.move("W", [0,6],[0,5])
-# b.display
-# b.move("W", [0,5],[0,4])
-# b.display
-# b.move("B", [0,3],[1,3])
-# b.display
-# b.move("B", [1,3],[2,3])
-# b.display
-# b.move("W", [0,4],[0,3])
-# b.display
-# p "Checkmate? #{b.checkmate?("B")}"
-# b.move("W", [7,0],[2,0])
-# b.display
-# b.move("W", [7,2],[5,4])
-# b.display
-# b.move("W", [5,4],[4,3])
-# b.display
-# b.move("W", [7,1],[5,0])
-# b.display
+
 # b.move("W", [5,0],[3,1])
 # b.display
 # p "Checkmate? #{b.checkmate?("B")}"
